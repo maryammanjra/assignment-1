8c32d4d0a24f000150af2a78987dc7d45ffdc8bf
Maryam Manjra <maryammanjra@gmail.com>
2025-08-16 15:17:11 -0400

    Move src

---
+++ src/data_structures.h
@@ -0,0 +1,33 @@
+#ifndef DATA_STRUCTURES_H
+#define DATA_STRUCTURS_H
+
+/*
+Modify this struct to accomodate your solutions
+*/
+
+typedef struct {
+    int PID;
+    int read_time; 
+    struct PCB *next;
+} PCB; 
+
+typedef struct {
+    int currProcess;
+} CPU;
+
+typedef struct {
+    PCB *head;
+    PCB *tail;
+    int length;
+} queue_t; 
+
+void enqueue(queue_t *add_to);
+
+PCB dequeue(queue_t *delete_from);
+
+PCB front(queue_t *get_front);
+
+void file_write(char *file_name, char *write_string);
+
+void file_read(char *file_name);
+#endif // DATA_STRUCTURES_H
---
+++ src/helper_functions.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <assert.h>
+
+/* Modify this function to store your input as you prefer, 
+DO NOT RETURN A CHAR ARRAY unless
+malloc'd and subsequently freed */ 
+void file_read(char *file_name){
+    FILE *file_ptr = fopen(file_name, "r");
+    assert(file_ptr != NULL);
+
+    int read_length = 30;
+    char curr_line[read_length];
+
+    while(fgets(curr_line, read_length, file_ptr)){
+        fprintf("%s", curr_line);
+    }
+
+    fclose(file_ptr);
+}
+
+void file_write(char *file_name, char *write_string){
+    FILE *file_ptr = fopen(file_name, "a");
+    assert(file_ptr != NULL);
+
+    fprintf(file_ptr, write_string);
+
+    fclose(file_ptr);
+}
+
+
+
